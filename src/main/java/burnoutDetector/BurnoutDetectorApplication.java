3e40749e4cad7ec4d9407fa26608c53cd7a7ff97
trivuniceva <trivunic99@gmail.com>
2025-08-20 15:03:26 +0200

    Initial commit

---
+++ src/main/java/burnoutDetector/BurnoutDetectorApplication.java
@@ -0,0 +1,13 @@
+package burnoutDetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurnoutDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurnoutDetectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/burnoutDetector/BurnoutDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package burnoutDetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurnoutDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
