a2ae541cb2a5979f72d0eeef3529378dea03eb7f
trivuniceva <trivunic99@gmail.com>
2025-08-25 19:15:42 +0200

    navbar and input style

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NgFor} from '@angular/common';
+import {InputComponent} from '../../shared/ui/input/input.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
-    NgFor
+    NgFor,
+    InputComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
