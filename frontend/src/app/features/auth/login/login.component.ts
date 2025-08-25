214ab8df8bf6964b40130e79ae9a80da5c2a137d
trivuniceva <trivunic99@gmail.com>
2025-08-25 18:01:28 +0200

    login ugrubo

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {InputComponent} from '../../../shared/ui/input/input.component';
+import {ButtonComponent} from '../../../shared/ui/button/button.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    InputComponent,
+    ButtonComponent
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  onLogin() {
+
+  }
 }
