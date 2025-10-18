04a93e08956d4530841474492a2928480da27ec3
trivuniceva <trivunic99@gmail.com>
2025-10-18 10:32:41 +0200

    .

---
+++ burnout-rules-engine/src/main/java/burnoutrulesengine/burnoutrulesengine/config/DroolsConfig.java
@@ -0,0 +1,9 @@
+package burnoutrulesengine.burnoutrulesengine.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DroolsConfig {
+
+
+}
---
+++ burnout-rules-engine/src/main/java/burnoutrulesengine/burnoutrulesengine/service/BurnoutRulesService.java
@@ -0,0 +1,4 @@
+package burnoutrulesengine.burnoutrulesengine.service;
+
+public class BurnoutRulesService {
+}
