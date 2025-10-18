c6011d4020f4a4abbebd8299dd4b8779a66076e0
trivuniceva <trivunic99@gmail.com>
2025-10-18 10:19:42 +0200

    rules engine

---
+++ burnout-rules-engine/src/main/java/burnoutrulesengine/burnoutrulesengine/BurnoutRulesEngineApplication.java
@@ -0,0 +1,13 @@
+package burnoutrulesengine.burnoutrulesengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurnoutRulesEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurnoutRulesEngineApplication.class, args);
+    }
+
+}
